f90195ed1e477b8bffbf6b1fc17296cd973be888
Preesha <psd33@cam.ac.uk>
2026-02-17 12:00:35 +0000

    created base layout jsx file and breadcrumb

---
+++ frontend/src/components/BaseLayout.jsx
@@ -0,0 +1,16 @@
+        function BaseLayout() {
+    return (
+
+        <div>
+            <div className="items-left px-6 py-4 bg-[#191d29] border-b">
+                    <header className="text-xl font-bold text-white">
+                        Calero Cloud Server Management
+                    </header>
+            </div>
+
+            </div>
+
+        );
+}
+
+export default BaseLayout;
