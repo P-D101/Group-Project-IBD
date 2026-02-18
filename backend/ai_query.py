dd67272a0cdfafa8c112092d9b3bf5413ce130b5
Preesha <psd33@cam.ac.uk>
2026-02-18 23:42:51 +0000

    added file

---
+++ backend/ai_query.py
@@ -0,0 +1,9 @@
+#code for the ai input box on home page, which will allow users to ask questions 
+#regarding how to use the platform
+
+#the result will be a natural langauge aanswer
+#along with a hyperlink to the relevant page if needed
+
+#could also allow users to ask questions about data 
+#this will have a natural language reponse along with relavant data from database
+
