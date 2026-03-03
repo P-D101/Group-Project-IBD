9a27ce59d6511ae08d1ccd0cdd8d9b39e87bc553
rilltan <rohandwadhawan@gmail.com>
2026-03-03 10:33:04 +0000

    basic table with api fetch

--- frontend/src/pages/ServiceViewer.jsx
+++ frontend/src/pages/ServiceViewer.jsx
@@ -1,33 +1,39 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 import Table from "../components/Table";
 
 function ServiceViewer() {
-    const providers = ['AWS', 'Oracle', 'Google Cloud', 'Microsoft']
-    
-    const headers = [{ txt: "Service Name", width: 1 },
-    { txt: "Billing (Monthly)", width: 2 },
-    { txt: "Billing (pp)", width: 1 },
-    { txt: "Quota usage%", width: 1 },
-    { txt: "Renewal date", width: 1 },
-    { txt: "Renewal period", width: 1 },
-    { txt: "Essential", width: 1 }]
+    const providers = ["AWS", "Oracle", "Google Cloud", "Microsoft"];
 
-    React.useEffect(() => {
-    fetch(`/api/usage/monthly?provider={}`)
-      .then(r => r.json())
-      .then(setRows);
-  }, []);
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => {
+        async function load() {
+            try {
+                const results = await Promise.all(
+                    providers.map(provider =>
+                        fetch(`http://localhost:5000/api/usage/monthly?provider=${encodeURIComponent(provider)}`)
+                            .then(r => {console.log(r); return r})
+                            .then(r => r.json())
+                            .then(data => [provider, data[0][1]] )
+                    )
+                );
+
+                setRows(results);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
+        load();
+    }, []);
 
     return (
         <div className="flex flex-col h-screen bg-gray-50">
-            <header className="flex justify-end items-center px-6 py-4 bg-white border-b border-gray-200 shadow-sm">
-                header section
-            </header>
 
             {/* Main Content Area */}
             <div className="flex gap-4 p-4 overflow-hidden flex-1">
                 {/* Center Services Table */}
-                <Table headers={headers} rows={rows}></Table>
+                <Table headers={[{txt:"Provider",width:1},{txt:"Monthly Usage",width:1}]} rows={rows}></Table>
             </div>
         </div>
     );
