285849354db8275e7440a66e39eab2aa79ab1874
AragornOfKebroyd <benjamin.tinsley54@gmail.com>
2026-03-09 21:19:37 +0000

    refactored scheduler a little

--- backend/VPL_Compute/scheduler.py
+++ backend/VPL_Compute/scheduler.py
@@ -6,57 +6,49 @@ import json
 from glob import glob
 import copy
 
-if __name__ == "__main__":
+dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))[:-12]
-    program_file_paths = glob(dir_path + "/data/programs/*.json")
+def load_programs(folder_name):
+    os.makedirs(os.path.join(dir_path, "data", folder_name), exist_ok=True)
+
+    program_file_paths = glob(os.path.join(dir_path, "data", folder_name, "*.json"))
 
     programs = []
-    running_programs = []
 
     for file_path in program_file_paths:
         with open(file_path, 'r') as program_file:
-            programs.append(convert_program(json.load(program_file)))
+            try:
+                prog = convert_program(json.load(program_file))
+                programs.append(prog)
+            except Exception as e:
+                print(f"Error in program {file_path}: {e}")
+    
+    return programs
 
 
+
+if __name__ == "__main__":
+    programs = load_programs("programs")
+    running_programs = []
+    
     while True:
 
         print('\033[92m' + ' Start Execute '.center(110, '-') + '\033[0m')
 
-        new_program_file_paths = glob(dir_path + "/data/new-programs/*.json")
-        
-        for file_path in new_program_file_paths:
-
+        new_programs = load_programs("new-programs")
+        programs.extend(new_programs)
+        # move all programs from new-programs to programs
+        for file_path in glob(os.path.join(dir_path, "data", "new-programs", "*.json")):
             file_name = file_path.split('/')[-1]
-
-            with open(file_path, 'r') as program_file:
-                programs.append(convert_program(json.load(program_file)))
-            
-            os.rename(file_path, dir_path + "/data/programs/" + file_name)
-
-        remove_program_file_paths = glob(dir_path + "/data/remove-programs/*.json")
-
-        for file_path in remove_program_file_paths:
-
-            file_name = file_path.split('/')[-1]
-
-            with open(file_path, 'r') as remove_file:
-                remove_object = json.load(remove_file)
-                for program in programs:
-                    if program["name"] == remove_object["Policy Name"]:
-                        programs.remove(program)
-
-            os.remove(file_path)
-            os.remove(dir_path + "/data/programs/" + file_name)
+            os.rename(file_path, os.path.join(dir_path, "data", "programs", file_name))
 
         running_programs = programs.copy()
 
-        disable_program_file_paths = glob(dir_path + "/data/disable-programs/*.json")
+        
+        disable_program_file_paths = glob(os.path.join(dir_path, "data", "disable-programs", "*.json"))
         
         for file_path in disable_program_file_paths:
-
             file_name = file_path.split('/')[-1]
-            
             with open(file_path, 'r') as remove_file:
                 disable_object = json.load(remove_file)
                 for program in programs:
